051f9a992beabcdcce4bff59ce6c257c07edd63f
Ferdef <theopulus@gmail.com>
2015-08-27 16:44:28 +0200

    Playing with API visibility

---
+++ common/models/no-rest-model.js
@@ -0,0 +1,23 @@
+module.exports = function(NoRestModel) {
+  NoRestModel.status = function(cb) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 20;
+    console.log('Current hour is ' + currentHour);
+    var response;
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = 'We are open for business';
+    } else {
+      response = 'Sorry, we are closed. Open daily from ' + OPEN_HOUR + ' to ' + CLOSE_HOUR;
+    }
+    cb(null, response);
+  };
+  NoRestModel.remoteMethod(
+    'status',
+    {
+      http: {path: '/status', verb: 'get'},
+      returns: {arg: 'status', type: 'string'}
+    }
+  );
+};
